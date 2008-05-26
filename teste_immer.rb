adcd2117136260c028856783a550fee7b9b2c880
Alto <boettger@mt7.de>
2008-05-26 21:28:19 +0200

    mit Inhalt

--- teste_immer.rb
+++ teste_immer.rb
@@ -1,5 +1,14 @@
-Warum?
-- damit du auch bei kleinen Änderungen noch weisst, dass du nichts kaputt gemacht hast
-- damit andere Entwickler wissen, dass sie nichts kaputt gemacht haben 
-- damit du weisst, was z.B. eine Unit eigentlich tuen soll (Verantwortung der Unit)
-  (und damit besseres Design)
+# Warum?
+# - weil es ein gutes Gefühl gibt
+# - damit du auch bei kleinen Änderungen noch weisst, dass du nichts kaputt gemacht hast
+# - damit andere Entwickler wissen, dass sie nichts kaputt gemacht haben 
+# - damit du weisst, was z.B. eine Unit eigentlich tuen soll (Verantwortung der Unit)
+#   (und damit besseres Design)
+
+# Probleme
+# - Testen kostet Zeit
+#   -> das sieht auf den ersten Blick so aus, ja, ist aber nicht so; die späteren Vorteile
+#      wiegen das mehr als auf
+# - ich weiss nicht, wie ich testen soll
+#   -> dafür sind wir ja jetzt hier ;-)
+
