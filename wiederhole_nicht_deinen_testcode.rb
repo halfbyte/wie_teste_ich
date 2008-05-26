acc1f4cf9f493bc13497c9b40144594cff96eb86
Alto <boettger@mt7.de>
2008-05-26 21:58:57 +0200

    Inhalt verbessert

--- wiederhole_nicht_deinen_testcode.rb
+++ wiederhole_nicht_deinen_testcode.rb
@@ -1,2 +1,9 @@
-- DRY ;-)
-- eigene Matcher und SharedSpecs
+# - eigene Matcher und SharedSpecs
+
+# Warum?
+# - um sich nicht zu wiederholen (DRY) ;-)
+
+# Probleme
+# - ich kann keine Metaprogrammierung
+#   -> braucht man ja gar nicht
+
