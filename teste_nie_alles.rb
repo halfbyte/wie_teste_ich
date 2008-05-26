f2e70ff741e43571d84e03ab24f1faeaacca97a8
Alto <boettger@mt7.de>
2008-05-26 21:30:24 +0200

    mit Inhalt

--- teste_nie_alles.rb
+++ teste_nie_alles.rb
@@ -1,12 +1,14 @@
-- sondern nur das Entscheidende
+# - sondern nur das Entscheidende
 
-Warum?
-- weil man sonst nicht mehr implementiert, sondern nur noch Tests schreibt; es kostet einfach zu viel Zeit!
-- nichts mehr tuen als nötig
-- gut genug...
+# Warum?
+# - weil man sonst nicht mehr implementiert, sondern nur noch Tests schreibt; es kostet 
+#   einfach zu viel Zeit!
+# - nichts mehr tuen als nötig
+# - gut genug...
 
-Probleme
-- wie finde ich das?
-  -> Beispiele für 'happy path' und Ausnahmen/Fehlerfälle
+# Probleme
+# - wie finde ich das? wann ist es gut genug?
+#   -> Erfahrung, Gefühl entwickeln (und Best Practices anwenden)
+#   -> Beispiele für 'happy path' und Ausnahmen/Fehlerfälle
 
   
