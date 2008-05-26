279d187d360919e5aa23f55051cafc6072c8b2a1
Alto <boettger@mt7.de>
2008-05-26 18:37:41 +0200

    rubyfied comments

--- teste_nur_deinen_code.rb
+++ teste_nur_deinen_code.rb
@@ -1,12 +1,13 @@
-- nichts Plugins, Gems oder gar Rails selber
+# - nicht Plugins, Gems oder gar Rails selber
+# * Keinen Framework-Code Testen
+#   * Assoziationen
+#     * reflect_on
+#   * Validations
+#     * validation_reflections
+
 # Warum?
 # - weil diese schon - zumindest meistens - gut genug getestet sind
 # - neu ist doch nur der eigene Code
 
 # Probleme:
 # - woran macht man fest, dass man nur den eigenen Code testet?
-* Keinen Framework-Code Testen
-  * Assoziationen
-    * reflect_on
-  * Validations
-    * validation_reflections
