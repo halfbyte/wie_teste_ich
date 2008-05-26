4b0c6a813b367291725e0e33003d8a1c188ffd93
Alto <boettger@mt7.de>
2008-05-26 20:53:09 +0200

    vermeide Fixtures

---
+++ vermeide_fixtures.rb
@@ -0,0 +1,8 @@
+# Warum?
+# - weil Fixtures wegen der DB-Zugriffe langsam sind
+# - weil Fixtures invalide Daten erlauben
+# - weil Fixtures, wenn groß,  schlecht zu pflegen sind (als YAML)
+# - weil Fixtures brüchig/zerbrechlich sind
+
+# Warum nicht?
+# - weil sie praktisch sind
