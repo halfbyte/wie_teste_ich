b7ac0d628a3d8565e98997f7230bf0e554e5608c
Alto <boettger@mt7.de>
2008-05-26 18:42:52 +0200

    rubyfied comments

--- teste_isoliert.rb
+++ teste_isoliert.rb
@@ -1,10 +1,9 @@
-Warum?
-- mock und stub, um externe System und lang laufende (oder forked) Prozesse zu vermeiden
+# Warum?
+# - mock und stub, um externe System und lang laufende (oder forked) Prozesse zu vermeiden
 
-Probleme
-- ich laufe Gefahr, zu sehr die Implementierung zu testen
-  -> finde ein gutes Mittelmaß zwischen zu viel und zu wenig Gemocke
-- wenn ich die Implementierung ändere, muss ich die Tests anpassen, obwohl 
-  sich die Schnittstelle bzw. das Verhalten nicht geändert haben
-  -> Mittelmaß finden!
-  
+# Probleme
+# - ich laufe Gefahr, zu sehr die Implementierung zu testen
+#   -> finde ein gutes Mittelmaß zwischen zu viel und zu wenig Gemocke
+# - wenn ich die Implementierung ändere, muss ich die Tests anpassen, obwohl 
+#   sich die Schnittstelle bzw. das Verhalten nicht geändert haben
+#   -> Mittelmaß finden!
