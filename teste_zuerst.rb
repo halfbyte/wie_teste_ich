18334e706675f8513ee2adcd6eaa6530731ba293
Alto <boettger@mt7.de>
2008-05-26 20:47:43 +0200

    rubyfied

--- teste_zuerst.rb
+++ teste_zuerst.rb
@@ -1,4 +1,11 @@
-Warum Test-Driven und nicht Test-Supported
+# - Test-Driven und nicht Test-Supported
 
-* Nachgelagerte Tests testen entweder zu viel oder zu wenig
-* Tests dienen der Reflektion über notwendiges Verhalten und notwendigen Code
+# Warum?
+# - weil man einen anderen Blickwinkel auf seine Anwendung bzw. auf die Schnittstelle hat;
+#   dient der Reflektion über notwendiges Verhalten und notwendigen Code
+# - weil man weniger die Implementierung testet, und eher das Verhalten; nachgelagerte 
+#   Tests testen entweder zu viel oder zu wenig
+
+# Probleme
+# - man testet zu integrativ und zu wenig isoliert
+#   -> Erfahrung hilft
