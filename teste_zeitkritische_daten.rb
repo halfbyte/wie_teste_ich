21726fe909e30b99f24e5a450a15ec3717085c01
Alto <boettger@mt7.de>
2008-05-27 23:04:55 +0200

    not needed anymore; integrated into teste_immer

--- teste_zeitkritische_daten.rb
+++
@@ -1,4 +0,0 @@
-Warum Test-Driven und nicht Test-Supported
-
-* Nachgelagerte Tests testen entweder zu viel oder zu wenig
-* Tests dienen der Reflektion über notwendiges Verhalten und notwendigen Code
