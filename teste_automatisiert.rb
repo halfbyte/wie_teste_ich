a8ae1c3914db434645256869760523fad485d476
Alto <boettger@mt7.de>
2008-05-28 12:28:30 +0200

    added a comment

--- teste_automatisiert.rb
+++ teste_automatisiert.rb
@@ -2,3 +2,5 @@
 # - weil wir alle schlurige Schlaffies sind
 # - man kann die automatisierte Testumgebung auch näher an der Produktionsumgebung laufen lassen
 #   (gleiche DB, gleiches OS, gleiche Gem-Versionen)
+
+# hier kann man auch die Migrationen testen (von Anfang an, und wenn man will auch die down-Richtung)
