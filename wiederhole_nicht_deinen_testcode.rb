3dc3785a01891fd64211818e3e932f01ed6353df
Alto <boettger@mt7.de>
2008-05-28 12:25:18 +0200

    added code

--- wiederhole_nicht_deinen_testcode.rb
+++ wiederhole_nicht_deinen_testcode.rb
@@ -7,5 +7,102 @@
 # - ich kann keine Metaprogrammierung
 #   -> braucht man ja gar nicht
 
-# TODO: Beispiel für Matcher [thorsten, 27.05.2008]
-# TODO: Beispiel für SharedSpecs [thorsten, 27.05.2008]
+# erstmal einen Shared-Spec
+
+# logo_spec.rb
+describe 'A logo' do
+  before do
+    @logo = Logo.new
+  end
+  it "should be valid with a valid content type" do
+    content_types = ['image/gif', 'image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg']
+    content_types.each do |content_type|
+      @logo.content_type = content_type
+      @logo.should be_valid
+    end
+  end
+  it "should be invalid without a valid content type" do
+    content_types = ['image/bmp','image/tif']
+    content_types.each do |content_type|
+      @logo.content_type = content_type
+      @logo.should_not be_valid
+    end
+  end
+end
+
+# besser:
+# image_spec.rb
+describe "a content type checker" do
+  it "should be valid with a valid content type", :shared => true do
+    content_types = ['image/gif', 'image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg']
+    content_types.each do |content_type|
+      @image.content_type = content_type
+      @image.should be_valid
+    end
+  end
+  it "should be invalid without a valid content type", :shared => true do
+    content_types = ['image/bmp','image/tif']
+    content_types.each do |content_type|
+      @image.content_type = content_type
+      @image.should_not be_valid
+    end
+  end
+end
+
+# logo_spec.rb
+describe 'A logo' do
+  before do
+    @image = Logo.new
+  end
+  it_should_behave_like 'a content type checker'
+end
+
+
+# --------------------------------------------------------------------------------------------------------
+# Beispiel für eigenen Matcher
+# user_spec.rb
+describe "A user" do
+  it "should have a name" do
+    user = User.new(:name => nil)
+    user.valid?
+    user.errors_on(:name).to_a.should include("can't be blank")
+  end
+end
+
+# have_validated_presence_of.rb
+module ActiveRecordMatchers
+  class HaveValidatedPresenceOf
+    def initialize(field)
+      @field=field
+    end
+
+    def matches?(model)
+      @model = model.new
+      @model.valid?
+      @model.errors.on(@field)
+      field_errors.each { |e| return true if e =~ /can't be blank/ }
+      false
+    end
+    
+    def description
+      "have validate_presence_of #{@field}"
+    end
+    def failure_message
+      "expected to validate presence_of #{@field}, but it doesn't"
+    end
+    def negative_failure_message
+      "not expected to validate presence_of #{@field}, but it does"
+    end
+  end
+
+  def have_validated_presence_of(field)
+    HaveValidatedPresenceOf.new(field)
+  end
+end
+
+# user_spec.rb
+describe "A user" do
+  it "should have a name" do
+    User.should have_validated_presence_of(:name)
+  end
+end
